9d59b341452785dba0ebec5e324cdf44147b8891
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-08-20 00:13:56 -0400

    Update Collections.tsx

--- components/Collections.tsx
+++ components/Collections.tsx
@@ -20,7 +20,7 @@ const Collections = () => {
               No Preservatives
             </a>
           </h3>
-          <p className="text-base font-semibold text-gray-900">100% organic and locally sourced grapes for NC & SC</p>
+          <p className="text-base font-semibold text-gray-900">100% ethically sourced grapes and fruits.</p>
         </div>
         <div className="group relative">
           <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
